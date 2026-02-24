f720ab76cd688dcb1aed03eb66845e812a3e689f
sofialaraferreiradossantos-ar <sofialaraferreiradossantos@gmail.com>
2026-02-24 11:18:50 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function  App() {
+  return(
+    <div>
+      <h1>Aula de React com Vite JS</h1>
+      <h2>Estilização com css</h2>
+      <h3>Eu não fiz nada</h3>
+      
+    </div>
+  )
+}
+ export default App;
+ 
