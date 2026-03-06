1520d657eaf215ed19aaf0b56f4e3ecdd6aae9d9
sofialaraferreiradossantos-ar <sofialaraferreiradossantos@gmail.com>
2026-03-06 14:45:56 -0300

    06/03
    aula 06

---
+++ src/pages/exemplos/ex-01/index.jsx
@@ -0,0 +1,9 @@
+function Exemplo01() {
+    return (
+        <div>
+            <h1> Exemplo 1</h1>
+        <p> Conteudo minimo para componente React </p>
+        </div>
+    )
+}
+export default Exemplo01;
---
+++ src/pages/exemplos/ex-02/index.jsx
@@ -0,0 +1,16 @@
+import styles from './index.module.css';
+
+function Exemplo02 () {
+    return(
+        <div className={styles.containerEx2}>
+            <h1>Exemplo 2</h1>
+            <h2>Estilização CSS</h2>
+            <h3>uso de CSS em diversos componentes</h3>
+            <p>Eu não fiz nada</p>
+            <label>Exemplo Labael</label>
+            <span>Exemplo span</span>
+
+        </div>
+    )
+}
+export default Exemplo02;
