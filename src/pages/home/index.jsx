fff8eb5a8545bbf3e4ca8b11f2e587f767336362
sofialaraferreiradossantos-ar <sofialaraferreiradossantos@gmail.com>
2026-03-13 14:40:40 -0300

    13/03
    aula 8

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,21 @@
+import {useNavigate } from 'react-router-dom';
+import { Link } from "react-router";
 function Home () {
+
+    const navigate = useNavigate();
+
     return(
         <div>
-            <h1>Home</h1>
+            <h1>Aula de React com Vite JS </h1>
+
+            <button
+            onClick={() => navigate('/exemplo/1')}
+            >
+            Exemplo 1
+            </button>
+
+          <Link to="/exemplo/2"> Exemplo 2</Link>
+
         </div>
     )
 }
